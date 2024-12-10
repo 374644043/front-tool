9fddb27d2494eb775c00139f3e36d7fe29f5d937
程序员A <pw374644043@gmail.com>
2024-12-10 19:58:03 +0800

    🐞 fix(路由): 尝试处理Github Pages路由问题

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,7 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   return {
-    base: process.env.VITE_BASE,
+    base: './',
     plugins: [
       vue(),
       AutoImport({
