169e03b60961442108d0dc4283f5203f759f18af
程序员A <pw374644043@gmail.com>
2024-12-10 22:07:27 +0800

    🐞 fix(路由配置): 处理Github Pages路由跳转问题

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,6 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => {
   return {
-    base: './',
     plugins: [
       vue(),
       AutoImport({
